a83bce71e19dfb9b7c9f2b8b99f2571efc5b0924
b01tech <bruno01.tech@gmail.com>
2025-07-15 08:08:33 -0300

    feat(migration): aplica migrations na inicializacao do servico

--- src/Catalog.Infrastructure/DependencyInjection/DependencyInjection.cs
+++ src/Catalog.Infrastructure/DependencyInjection/DependencyInjection.cs
@@ -2,9 +2,11 @@
 using Catalog.Infrastructure.Data;
 using Catalog.Infrastructure.Repositories;
 using MassTransit;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Catalog.Infrastructure.DependencyInjection;
 public static class DependencyInjection
@@ -16,6 +18,25 @@ public static class DependencyInjection
         AddMassTransit(services);
 
         return services;
+    }
+
+    public static IHost ApplyMigrations(this IHost app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var servicesProvider = scope.ServiceProvider;
+            var dbContext = servicesProvider.GetRequiredService<AppDbContext>();
+            try
+            {
+                if (dbContext.Database.GetPendingMigrations().Any())
+                    dbContext.Database.Migrate();
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine($"Falha ao conectar com banco de dados | {ex.Message}");
+            }
+        }
+        return app;
     }
 
     private static void AddDbContext(IServiceCollection services, IConfiguration config)
